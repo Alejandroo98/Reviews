c86ea8fa721ad1e04a1dcfa155fa3a9ddbc09c1b
Alejandro <jeffersonalejandro27@gmail.com>
2020-10-11 16:04:19 -0500

    Ejerciio de tablas de multiplicar. (Seccion 4 C. 38

--- Node.js/1/multiplicar/multiplicar.js
+++ Node.js/1/multiplicar/multiplicar.js
@@ -1,9 +1,35 @@
 const fs = require('fs');
+// const colors = require('colors');
+var colors = require('colors/safe');
 const { resolve } = require('path');
 
 let data = ""
 
-let tradicionalPRomise = (base) => {   //module.exports.tradicionalPRomise -> Es lo mismo que  modulo.exports = { tradicionalPRomise }
+let listarTabla = ( base , limite = 10 ) => {
+    return new Promise ((resolve , reject ) => {
+        if(!Number(base)){
+            reject(`${base} No es un numero`)
+            return
+        }
+
+        for (let i = 1; i <= limite ; i++) {
+            data +=  ( `${base} * ${i} = ${ base * i}\n`);
+        };
+        console.log("==================".red);
+        console.log("Tabla creada".blue);
+        console.log("==================".red);
+        
+        fs.writeFile(`tablas/tabla - ${base}.txt`, data, (error) => {   //tablas/ es la carpeta /tabla es el nombre del archivo , fata es el archivo que queremos guardar , (err) es el calck que quiero que se ejecute en cuanto ocurra un error
+            if (error) 
+            reject(error); 
+            else
+            resolve(`tabla - ${base}.txt`);
+        });
+    })
+}
+
+
+/* let tradicionalPRomise = (base) => {   //module.exports.tradicionalPRomise -> Es lo mismo que  modulo.exports = { tradicionalPRomise }
 
     return new Promise ((resolve , reject ) => {
         if(!Number(base)){
@@ -11,7 +37,7 @@ let tradicionalPRomise = (base) => {   //module.exports.tradicionalPRomise -> Es
             return
         }
 
-        for (let i = 1; i <= 10 ; i++) {
+        for (let i = 1; i <= limite ; i++) {
             data +=  ( `${base} * ${i} = ${ base * i}\n`);
         };
         
@@ -23,7 +49,10 @@ let tradicionalPRomise = (base) => {   //module.exports.tradicionalPRomise -> Es
         });
     })
         
-};
+}; */
+
+
+
 
 /* let suma = (num1 , num2) => {
     return `${num1} + ${num2} = ${num1 + num2}`
@@ -32,7 +61,8 @@ let tradicionalPRomise = (base) => {   //module.exports.tradicionalPRomise -> Es
 
 
 module.exports = {
-    tradicionalPRomise  : tradicionalPRomise,  //Tambien podriamos poner solamente tradicionalPRomise
+    // tradicionalPRomise  : tradicionalPRomise,  //Tambien podriamos poner solamente tradicionalPRomise
+    listarTabla
 };
 //Repetir capitulo 30
 
